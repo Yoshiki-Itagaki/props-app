6a6efa73721f308fb50af1bcf2eaa7423f65c5ba
Yoshiki-Itagaki <b1700336@gl.aiu.ac.jp>
2024-02-11 17:16:32 +0900

    Create a new React.js app

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+
+        {
+            contacts.map(contact =>
+                <Card
+                    name = {contact.name}
+                    img = {contact.imgURL}
+                    tel = {contact.phone}
+                    email = {contact.email}
+                />    
+            )
+        }
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/contacts.js
@@ -0,0 +1,32 @@
+const contacts = [
+    {
+      name: "Beyonce",
+      imgURL:
+        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      phone: "+123 456 789",
+      email: "b@beyonce.com"
+    },
+    {
+      name: "Jack Bauer",
+      imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com"
+    },
+    {
+      name: "Chuck Norris",
+      imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com"
+    },
+    {
+      name: "Natalie Portman",
+      imgURL:
+        "https://cdn.tatlerasia.com/asiatatler/i/sg/2018/11/05145633-natalie-portman_cover_1280x853.jpg",
+      phone: "+967 371 094",
+      email: "gmail@natalieportman.com"
+    }
+  ];
+  
+  export default contacts
