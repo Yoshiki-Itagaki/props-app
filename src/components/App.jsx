2c8d9c83b81dc9379c0434a5965883cb4a64f7e2
Yoshiki-Itagaki <b1700336@gl.aiu.ac.jp>
2024-02-11 18:27:12 +0900

    Implement the data mapping functionality to the app.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "./Card";
+import Avatar from "./Avatar";
 import contacts from "../contacts";
+
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
+      <Avatar
+        img="https://www.southparkshop.com/cdn/shop/products/SP-Cartman-Stand-40_800x.jpg?v=1618604102"
+      />
         {
             contacts.map(contact =>
                 <Card
